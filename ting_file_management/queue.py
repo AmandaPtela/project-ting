ce70fe20ac90ae8c6f32b951792812789332a6a9
Amanda Portela <mandaptela@gmail.com>
2023-07-20 20:39:35 -0300

    Requisito 1 - Queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,28 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.queue = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if len(self.queue) < 1:
+            return None
+        value = self.queue[-1]
+        print('LISTA DE VALORES ANTES', self.queue, self.queue.index(value))
+
+        del self.queue[self.queue.index(value)]
+        print('LISTA DE VALORES DEPOIS', self.queue, self.queue[-1])
+        # self.queue[-1]
+        return self.queue[0]
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        # print('VÁLIDO TEM QUE SER ENTRE', range(0, len(self.queue) - 1))
+        # print(index)
+        if index >= len(self.queue) or index < 0:
+            raise IndexError("Índice Inválido ou Inexistente")
+        return self.queue[index]
