a8f27b670dba1126e539a3be19923b3db0a701d4
Amanda Portela <mandaptela@gmail.com>
2023-08-02 19:57:05 -0300

    Requisito 2 - file management

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,3 +1,19 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
-    # Iniciar
+    string_path = str(path_file)  # String
+    arquivo = None  # arquivo/string separada vem p cá p ser retornada no fim
+
+    if not string_path.endswith('.txt'):  # se final não for .txt
+        print('Formato inválido', file=sys.stderr)
+    try:
+        with open(path_file, 'r') as Arquivo:  # abrir arquivo p leitura nomean
+            data = Arquivo.read().split('\n')  # Dividindo a stringona/arquivo
+            arquivo = data  # define a stringona como arquivo pra retornar
+            print(data)
+    except FileNotFoundError:  # Caso arquivo não encontrado lança a exceção
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        print(arquivo)
+
+    return arquivo  # arquivo criado e com valor associado no TRY
