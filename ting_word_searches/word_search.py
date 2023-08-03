1419c0185ae9e97cb60910ab22372987c91a8d8b
Amanda Portela <mandaptela@gmail.com>
2023-08-02 22:14:21 -0300

    inicio requisito word_search

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,27 @@
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
+    lista = instance.queue
+
+    if not word:
+        return []
+
+    for i in range(len(lista)):
+        if word not in lista[i]["linhas_do_arquivo"]:
+            return []
+
+        return [{
+            "palavra": word,
+            "arquivo": lista[i]["nome_do_arquivo"],
+            "ocorrencias": 2
+        }]
+
+
+""" dict_found = {
+            "palavra": word,
+            "arquivo": lista["nome_do_arquivo"],
+            "ocorrencias"
+        }
+        print("palavra":) """
 
 
 def search_by_word(word, instance):
